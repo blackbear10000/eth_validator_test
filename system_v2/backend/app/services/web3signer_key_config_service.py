c47726d4978764b6892138102c75ec9299e3e3db
blackbear10000 <shuaishuai.qc@gmail.com>
2025-12-05 17:17:29 +0800

    Fix bugs

--- system_v2/backend/app/services/web3signer_key_config_service.py
+++ system_v2/backend/app/services/web3signer_key_config_service.py
@@ -191,14 +191,20 @@ class Web3SignerKeyConfigService:
             
             # 保存 YAML 文件
             # 使用自定义的 Representer 确保所有字符串值都用双引号（符合 Web3Signer 官方文档格式）
+            # 注意：键不使用引号，只有值使用双引号
             def quoted_str_presenter(dumper, data):
-                """自定义字符串表示器，使用双引号"""
+                """自定义字符串表示器，使用双引号（仅用于值）"""
                 return dumper.represent_scalar('tag:yaml.org,2002:str', data, style='"')
             
             # 创建自定义 Dumper
             class Web3SignerDumper(yaml.SafeDumper):
-                pass
+                def represent_mapping(self, tag, mapping, flow_style=None):
+                    """重写 mapping 表示器，确保键不使用引号"""
+                    # 调用父类方法，但确保键不使用引号
+                    return super().represent_mapping(tag, mapping, flow_style=False)
             
+            # 只对字符串值使用双引号，键不使用引号
+            # 注意：PyYAML 默认键不使用引号，所以只需要为值添加引号
             Web3SignerDumper.add_representer(str, quoted_str_presenter)
             
             # 确保所有值都是字符串类型（符合 Web3Signer 要求）
